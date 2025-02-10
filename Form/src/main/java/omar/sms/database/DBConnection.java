ba5c8f1cd313d82e2e4edb1a17dfc3b1d0ebfde0
Omar Faruq <omarfaruq9723@gmail.com>
2025-02-10 18:45:32 +0600

    JSP Servlet initial

---
+++ Form/src/main/java/omar/sms/database/DBConnection.java
@@ -0,0 +1,23 @@
+package omar.sms.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	private static final String URL = "jdbc:postgresql://localhost:5432/DemoDB";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "1234";
+
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("driver not found!", e);
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+}
